2c7dedcc55ab18756799906edc290c7afcba1b7e
kadarme <kn.adarme@gmail.com>
2015-02-12 17:44:59 +0000

    Attempted to create authentication from scratch.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   get 'welcome/about'
 
   root to: 'welcome#index'
+  
 end
