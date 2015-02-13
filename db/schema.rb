0429bb86b742de57ddb685f7acbbebeebc7a3a07
kadarme <kn.adarme@gmail.com>
2015-02-13 03:00:39 +0000

    Had to rollback the database because I was unable to create the User database on another branch.

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150210192229) do
-
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
