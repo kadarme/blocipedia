01a68c5cccdd88010072cdf589b630043409ef9c
kadarme <kn.adarme@gmail.com>
2015-02-13 05:02:11 +0000

    Added devise, capybara, and rspec.

---
+++ spec/features/user_signs_up_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+feature 'User signs up for Blocipedia' do
+  scenario 'Successfully' do
+    visit new_user_registration_path
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: 'helloworld'
+    fill_in 'Password confirmation', with: 'helloworld'
+    click_button 'Sign up'
+    expect(page).to have_content('A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.')
+  end
+end
---
+++ spec/features/welcome_pages_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+feature 'App opens to welcome page' do
+  scenario 'Successfully' do
+    visit root_path
+    expect(page).to have_content('Home About Sign In or Sign Up')
+  end
+end
+  
